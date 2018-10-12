313eff5d86cbca5c3c2264290c186d36f07b1598
Paul Mathis <paul.mathis@gmail.com>
2018-10-11 20:46:28 -0500

    added search and signin to header

---
+++ src/components/Header/SearchSign/Button.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background-color: grey;
+  color: white;
+  border: none;
+  text-decoration: none;
+  padding: 3px 6px;
+`;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
 import NavItem from './NavItem';
+import SearchSign from './SearchSign';
 
 const Header = styled.header`
   text-align: center;
+  position: relative;
 `;
 
 export default () => {
   return (
     <Header>
       <h1>Plantain Commonwealth</h1>
+      <SearchSign />
       <Nav>
         <NavItem>New Arrivals</NavItem>
         <NavItem>Women</NavItem>
