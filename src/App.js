d682126caf54494602ac29fbc85d3a449c350aea
Paul Mathis <paul.mathis@gmail.com>
2018-10-14 19:12:19 -0500

    added container component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Container from './components/Container';
 import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Header />
-      </div>
+      </Container>
     );
   }
 }
