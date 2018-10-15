3b5a7b59dbeed3d1d6c8a0a26039594f3f21699b
Paul Mathis <paul.mathis@gmail.com>
2018-10-14 19:43:21 -0500

    Added hero image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Container from './components/Container';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 class App extends Component {
   render() {
     return (
       <Container>
         <Header />
+        <Hero />
       </Container>
     );
   }
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import hero from '../assets/hero.jpg';
+
+export default styled.section`
+  background-image: url(${hero});
+  background-size: contain;
+  height: 640px;
+  width: 100%;
+`;
