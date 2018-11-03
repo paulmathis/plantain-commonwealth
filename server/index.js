2902178bddd5c939ceea268218847f10c9e7a433
Paul Mathis <paul.mathis@gmail.com>
2018-11-03 18:26:01 -0500

    edited config to lint and format client and server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 5000;
 
 // app.get("/", (req, res) => res.send("Hello World!"));
 
 // create a GET route
-app.get("/express_backend", (req, res) => {
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+/* eslint-disable */
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
