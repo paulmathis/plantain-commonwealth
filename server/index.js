d0571820bd6ec566aa494dd4585fbd9de869ab4c
Paul Mathis <paul.mathis@gmail.com>
2018-11-03 17:20:03 -0500

    Moved frontend into client folder and created a simple express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+// create a GET route
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
